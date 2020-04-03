8ae7d0b76c765bb4ebcda5224afaa2a17b62f80d
Hovhannes Sahakyan <syanhovhannes@gmail.com>
2020-04-03 19:20:03 +0400

    fix: [change] server port to 8080

--- server.js
+++ server.js
@@ -10,6 +10,6 @@ app.post('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(8080, function () {
+  console.log('Example app listening on port 8080!');
 });
